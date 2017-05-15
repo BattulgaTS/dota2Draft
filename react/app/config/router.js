8f58e9640c324129de973a3d26eec599d68875aa
tulga <ts.battulga18@gmail.com>
2017-05-15 07:35:24 -0700

    123

--- react/app/config/router.js
+++ react/app/config/router.js
@@ -7,7 +7,39 @@ var IndexRoute = ReactRouter.IndexRoute;
 var Main = require('../component/main');
 var Home = require('../component/home');
 var PromptContainer = require('../container/promptcontainer');
-var List = require('../container/list');
+// var List 			= require('../container/list');
+
+// var React = require('react');
+var PropTypes = React.PropTypes;
+var transparentBg = require('../styles').transparentBg;
+
+function List (props){
+  return (
+    <div className='jumbotron col-sm-6 col-sm-offset-3 text-center' style={transparentBg}>
+      <h1> Add hero to Enemy team </h1>
+      <div className='col-sm-12'>
+        <form>
+
+          <div className='form-group'>
+            <input
+              className='form-control'
+              placeholder='Hero Name'
+              type='text' />
+          </div>
+
+          <div className='form-group col-sm-4 col-sm-offset-4'>
+            <button
+              className='btn btn-block btn-success'
+              type='submit' >
+              Add
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
+
+}
 
 var routes = (
   <Router history={hashHistory}>
